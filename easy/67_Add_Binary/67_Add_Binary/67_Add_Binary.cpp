c4cb54ec2858e6132e19917eba9405d126afcc00
xixuecao <xixuecao@gmail.com>
2019-10-01 13:06:13 +0800

    67 Add Binary

---
+++ easy/67_Add_Binary/67_Add_Binary/67_Add_Binary.cpp
@@ -0,0 +1,54 @@
+﻿// 67_Add_Binary.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string addBinary(string a, string b) {
+		string res = "";
+		int m = a.size() - 1, n = b.size() - 1, carry = 0;
+		while (m >= 0 || n >= 0)
+		{
+			int p = m >= 0 ? a[m--] - '0' : 0;
+			int q = n >= 0 ? b[n--] - '0' : 0;
+			int sum = p + q + carry;
+			res = to_string(sum % 2) + res;
+			carry = sum / 2;
+		}
+		return carry == 1 ? "1" + res : res;
+
+		/*
+	    string res = "";
+        int m = a.size() - 1, n = b.size() - 1, carry = 0;
+        while (m >= 0 || n >= 0) {
+            int p = m >= 0 ? a[m--] - '0' : 0;
+            int q = n >= 0 ? b[n--] - '0' : 0;
+            int sum = p + q + carry;
+            res = to_string(sum % 2) + res;
+            carry = sum / 2;
+        }
+        return carry == 1 ? "1" + res : res;
+		*/
+	}
+};
+
+int main()
+{
+	string a = "11", b = "1";
+	Solution s;
+	cout << s.addBinary(a,b) << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
