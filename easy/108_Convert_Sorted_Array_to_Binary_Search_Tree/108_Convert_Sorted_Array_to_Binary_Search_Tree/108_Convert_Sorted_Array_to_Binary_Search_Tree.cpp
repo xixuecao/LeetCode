8aef52bf8f1c6537d04f193e59e1108542b8c1e9
xixuecao <xixuecao@gmail.com>
2019-10-11 07:58:30 +0800

    108 Convert Sorted Array to Binary Search Tree

---
+++ easy/108_Convert_Sorted_Array_to_Binary_Search_Tree/108_Convert_Sorted_Array_to_Binary_Search_Tree/108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
@@ -0,0 +1,47 @@
+﻿// 108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+ 
+class Solution {
+public:
+	TreeNode* sortedArrayToBST(vector<int>& nums) {
+		if (nums.empty()) return NULL;
+		int mid = nums.size() / 2;
+		TreeNode* cur = new TreeNode(nums[mid]);
+		vector<int> left(nums.begin(), nums.begin() + mid), right(nums.begin() + mid + 1, nums.end());
+		cur->left = sortedArrayToBST(left);
+		cur->right = sortedArrayToBST(right);
+		return cur;
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<int> vi{ -10,-3,0,5,9 };
+
+	s.sortedArrayToBST(vi);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
