0089e2657d10d88192f7bad064c5cb088c34b272
xixuecao <xixuecao@gmail.com>
2020-01-01 08:51:24 +0800

    61 Rotate List

---
+++ medium/61_Rotate_List/61_Rotate_List/61_Rotate_List.cpp
@@ -0,0 +1,47 @@
+﻿// 61_Rotate_List.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* rotateRight(ListNode* head, int k) {
+		if (!head) return NULL;
+		int len = 1;
+		ListNode* cur = head;
+		while (cur->next)
+		{
+			len++;
+			cur = cur->next;
+		}
+		k %= len;
+		cur->next = head;
+		for (int i = 0; i < len - k; i++) cur = cur->next;
+		ListNode* res = cur->next;
+		cur->next = NULL;
+		return res;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
