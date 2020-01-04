af6b69f441c8655aff77e6151c90c423253ef17a
xixuecao <xixuecao@gmail.com>
2020-01-04 08:48:10 +0800

    64 Minimum Path Sum

---
+++ medium/64_Minimum_Path_Sum/64_Minimum_Path_Sum/64_Minimum_Path_Sum.cpp
@@ -0,0 +1,55 @@
+﻿// 64_Minimum_Path_Sum.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left 
+
+to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example:
+
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+*/
+using namespace std;
+
+class Solution {
+public:
+	int minPathSum(vector<vector<int>>& grid) {
+		int m = grid.size(), n = grid[0].size();
+		for (int i = 1; i < m; i++) grid[i][0] += grid[i - 1][0];
+		for (int i = 1; i < n; i++) grid[0][i] += grid[0][i - 1];
+		for (int i = 1; i < m; i++)
+			for (int j = 1; j < n; j++)
+				grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+		return grid[m - 1][n - 1];
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
