a033bff8ec15eae2fdd2821c3131ab2a23a57cc4
xixuecao <xixuecao@gmail.com>
2020-01-23 12:06:45 +0800

    91 Decode Ways

---
+++ medium/91_Decode_Ways/91_Decode_Ways/91_Decode_Ways.cpp
@@ -0,0 +1,41 @@
+﻿// 91_Decode_Ways.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int numDecodings(string s) {
+		if (s.empty() || s[0] == '0') return 0;
+		vector<int> dp(s.size() + 1, 0);
+		dp[0] = 1;
+		for (int i = 1; i < dp.size(); i++)
+		{
+			dp[i] = (s[i - 1] == '0') ? 0 : dp[i - 1];
+			if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6')))
+			{
+				dp[i] += dp[i - 2];
+			}
+		}
+		return dp.back();
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
