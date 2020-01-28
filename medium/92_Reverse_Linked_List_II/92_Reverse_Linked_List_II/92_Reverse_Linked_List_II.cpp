9fccbeef27f994f33780d14330a6cc215ebbbe9f
xixuecao <xixuecao@gmail.com>
2020-01-28 11:43:48 +0800

    92 Reverse Linked List II

---
+++ medium/92_Reverse_Linked_List_II/92_Reverse_Linked_List_II/92_Reverse_Linked_List_II.cpp
@@ -0,0 +1,48 @@
+﻿// 92_Reverse_Linked_List_II.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+		if (!head) return NULL;
+		ListNode* pre = new ListNode(-1), *node = pre;
+		pre->next = head;
+		for (int i = 0; i < m - 1; i++) node = node->next;
+		ListNode* cur = node->next;
+		for (int i = m; i < n; i++)
+		{
+			ListNode* temp = cur->next;
+			cur->next = temp->next;
+			temp->next = node->next;
+			node->next = temp;
+		}
+		return pre->next;
+
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
