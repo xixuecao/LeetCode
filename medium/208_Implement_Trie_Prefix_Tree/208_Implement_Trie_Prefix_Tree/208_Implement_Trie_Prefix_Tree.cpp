cd8c9bb2a1dcb0ee9e7d8117fe0ac744ba1f071f
xixuecao <xixuecao@gmail.com>
2020-03-15 08:03:18 +0800

    208 Implement Trie Prefix Tree

---
+++ medium/208_Implement_Trie_Prefix_Tree/208_Implement_Trie_Prefix_Tree/208_Implement_Trie_Prefix_Tree.cpp
@@ -0,0 +1,68 @@
+﻿// 208_Implement_Trie_Prefix_Tree.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode* child[26];
+    bool isWord;
+    TrieNode() : isWord(false) {
+        for (auto& a : child) a = nullptr;
+    }
+};
+
+class Trie {
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    void insert(string s) {
+        TrieNode* p = root;
+        for (auto& a : s) {
+            int i = a - 'a';
+            if (!p->child[i]) p->child[i] = new TrieNode();
+            p = p->child[i];
+        }
+        p->isWord = true;
+    }
+    bool search(string key) {
+        TrieNode* p = root;
+        for (auto& a : key) {
+            int i = a - 'a';
+            if (!p->child[i]) return false;
+            p = p->child[i];
+        }
+        return p->isWord;
+    }
+    bool startsWith(string prefix) {
+        TrieNode* p = root;
+        for (auto& a : prefix) {
+            int i = a - 'a';
+            if (!p->child[i]) return false;
+            p = p->child[i];
+        }
+        return true;
+    }
+
+private:
+    TrieNode* root;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
