af55073d642103206899d9084f0b9ec0f58e5838
xixuecao <xixuecao@gmail.com>
2020-03-20 08:06:54 +0800

    211 Add and Search Word

---
+++ medium/211_Add_and_Search_Word/211_Add_and_Search_Word/211_Add_and_Search_Word.cpp
@@ -0,0 +1,54 @@
+﻿// 211_Add_and_Search_Word.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <unordered_set>
+#include <regex>
+
+using namespace std;
+
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    WordDictionary() {
+
+    }
+
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        strs.insert(word);
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        if (strs.count(word) != 0) return true;
+        else {
+            regex e(word);
+            for (auto s:strs)
+            {
+                if (regex_match(s, e)) return true;
+            }
+        }
+        return false;
+    }
+private:
+    unordered_set<string> strs;
+};
+
+int main()
+{
+    string s = "word";
+    regex e("word");
+    if (regex_match(s, e)) cout << "succeed" << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
