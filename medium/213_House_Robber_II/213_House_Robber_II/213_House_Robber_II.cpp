1f51d5100909aea0a9d92513cd6ef06d65ae8fd5
xixuecao <xixuecao@gmail.com>
2020-03-22 08:07:36 +0800

    213 House Robber II

---
+++ medium/213_House_Robber_II/213_House_Robber_II/213_House_Robber_II.cpp
@@ -0,0 +1,45 @@
+﻿// 213_House_Robber_II.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() <= 1) return nums.empty() ? 0 : nums[0];
+        return max(rob(nums, 0, nums.size() - 1), rob(nums, 1, nums.size()));
+    }
+    int rob(vector<int>& nums, int l, int r) {
+        if (r - l <= 1) return nums[l];
+        vector<int> dp(r);
+        dp[l] = nums[l];
+        dp[l + 1] = max(nums[l], nums[l + 1]);
+        for (int i = l+2; i < r; i++)
+        {
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp.back();
+    }
+};
+
+int main()
+{
+    vector<int> vi = { 1,2,3,1 };
+    Solution sol;
+    cout << sol.rob(vi) << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
