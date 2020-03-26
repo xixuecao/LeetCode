5020f973b990873382e61fa9c85b7f3810531d3d
xixuecao <xixuecao@gmail.com>
2020-03-26 08:32:46 +0800

    221 Maximal Square

---
+++ medium/221_Maximal_Square/221_Maximal_Square/221_Maximal_Square.cpp
@@ -0,0 +1,45 @@
+﻿// 221_Maximal_Square.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) return 0;
+        int m = matrix.size(), n = matrix[0].size(),res = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 || j == 0) 
+                    dp[i][j] = matrix[i][j] - '0';
+                else if (matrix[i][j] == '1') 
+                    dp[i][j] = min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                res = max(res, dp[i][j]);
+            }
+        }
+        return res * res;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
