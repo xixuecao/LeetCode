497e896c580daafbd1ac99dab638ee8f8b27e204
xixuecao <xixuecao@gmail.com>
2020-04-15 08:28:37 +0800

    264 Ugly Number II

---
+++ medium/264_Ugly_Number_II/264_Ugly_Number_II/264_Ugly_Number_II.cpp
@@ -0,0 +1,42 @@
+﻿// 264_Ugly_Number_II.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> res{ 1 };
+        int i2 = 0, i3 = 0, i5 = 0;
+        while (res.size() < n)
+        {
+            int m2 = res[i2] * 2, m3 = res[i3] * 3, m5 = res[i5] * 5;
+            int mm = min(m2, min(m3, m5));
+            if (mm == m2) i2++;
+            if (mm == m3) i3++;
+            if (mm == m5) i5++;
+            res.push_back(mm);
+        }
+        return res.back();
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
